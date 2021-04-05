a6c3912802d421390d0de91bdebe64a7c5a86aaa
Isabel Alvarado Blanco Uribe <alvaradoblancouribe@gmail.com>
2021-04-05 19:06:40 -0400

    update

--- bits.c
+++ bits.c
@@ -280,7 +280,7 @@ int divpwr2(int x, int n) {
  *   Rating: 3
  */
 int isNonNegative(int x) {
-  return (~(!!(x>>2)));
+  return (!!(x>>1));
 }
 
 /*
@@ -307,7 +307,7 @@ int satMul2(int x) {
  *   Rating: 3
  */
 int isLess(int x, int y) {
-  return ((x+(~y+1))>>31);
+  return ~((x+(~y+1))>>31);
 }
 /* 
  * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0' to '9')
